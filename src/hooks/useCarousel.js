417fff88dc54f75915c886268577873a622f493e
Riley Brown <riley@riley.gg>
2020-06-07 16:27:38 -0400

    added arrow keys carousel navigation feature, added jc doc types, adjusted isPaused boolean naming

--- src/hooks/useCarousel.js
+++ src/hooks/useCarousel.js
@@ -1,24 +1,48 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from 'react';
 
-export default function useCarousel({ dataArray, interval } = {}) {
+/** @param {Object} obj - Object of arguments
+ * @param {any[]} obj.dataArray - Array of items used to increment and decrement carousel index
+ * @param {number} obj.interval - Time in milliseconds to increment carousel index
+ * @param {boolean} obj.allowArrowKeysNavigation - Boolean to allow navigating the carousel with keyboard arrow keys
+ */
+export default function useCarousel({
+  dataArray,
+  interval,
+  allowArrowKeysNavigation
+}) {
   const [index, setIndex] = useState(0);
-  const [paused, setPaused] = useState(false);
+  const [isPaused, setIsPaused] = useState(false);
 
+  /** @type React.MutableRefObject<number> */
   const intervalRef = useRef();
 
   useEffect(() => {
-    if (!paused) {
-      intervalRef.current = setInterval(increment, interval);
+    if (!isPaused) {
+      intervalRef.current = window.setInterval(increment, interval);
     }
-    return () => clearInterval(intervalRef.current);
-  }, [paused, interval, index]);
+    if (allowArrowKeysNavigation) {
+      window.addEventListener('keydown', arrowKeysNavigation);
+    }
+    return () => {
+      clearInterval(intervalRef.current);
+      window.removeEventListener('keydown', arrowKeysNavigation);
+    };
+  }, [isPaused, interval, index, allowArrowKeysNavigation]);
+
+  const arrowKeysNavigation = (e) => {
+    if (e.key === 'ArrowRight' || e.keyCode === 39) {
+      next();
+    } else if (e.key === 'ArrowLeft' || e.keyCode === 37) {
+      previous();
+    }
+  };
 
   // increment index, reset to 0 when index is on last item
   const increment = () => {
     if (index >= dataArray.length - 1) {
       setIndex(0);
     } else {
-      setIndex(index => index + 1);
+      setIndex((index) => index + 1);
     }
   };
 
@@ -28,20 +52,19 @@ export default function useCarousel({ dataArray, interval } = {}) {
     if (index === 0) {
       setIndex(dataArray.length - 1);
     } else {
-      setIndex(index => index - 1);
+      setIndex((index) => index - 1);
     }
   };
 
   const pause = () => {
-    setPaused(true);
+    setIsPaused(true);
     clearInterval(intervalRef.current);
   };
 
   const resume = () => {
-    setPaused(false);
-    let int = setInterval(increment, interval);
-    intervalRef.current = int;
+    setIsPaused(false);
+    intervalRef.current = window.setInterval(increment, interval);
   };
 
-  return { index, next, previous, pause, paused, resume };
+  return { index, next, previous, pause, isPaused, resume };
 }
